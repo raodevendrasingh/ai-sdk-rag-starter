0dd63cc00187a2d9c87893fae4b35d25055d6696
Devendra Singh Rao <devthedeveloperguy@gmail.com>
2025-02-28 12:28:32 +0530

    feat: upgrade tailwind and add prettier formatting

--- lib/actions/resources.ts
+++ lib/actions/resources.ts
@@ -1,24 +1,26 @@
-'use server';
+"use server";
 
 import {
-  NewResourceParams,
-  insertResourceSchema,
-  resources,
-} from '@/lib/db/schema/resources';
-import { db } from '../db';
+	NewResourceParams,
+	insertResourceSchema,
+	resources,
+} from "@/lib/db/schema/resources";
+import { db } from "../db";
 
 export const createResource = async (input: NewResourceParams) => {
-  try {
-    const { content } = insertResourceSchema.parse(input);
+	try {
+		const { content } = insertResourceSchema.parse(input);
 
-    const [resource] = await db
-      .insert(resources)
-      .values({ content })
-      .returning();
+		const [resource] = await db
+			.insert(resources)
+			.values({ content })
+			.returning();
 
-    return 'Resource successfully created.';
-  } catch (e) {
-    if (e instanceof Error)
-      return e.message.length > 0 ? e.message : 'Error, please try again.';
-  }
+		return "Resource successfully created.";
+	} catch (e) {
+		if (e instanceof Error)
+			return e.message.length > 0
+				? e.message
+				: "Error, please try again.";
+	}
 };
